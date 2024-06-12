7bbcf7f3373734120d70378d036108f936de2f0e
Sam Hastings <samhastingsis@gmail.com>
2024-06-12 16:49:14 -0400

    Initial Commit

---
+++ SwiftDataInBackground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataInBackground
+//
+//  Created by sam hastings on 12/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataInBackground/SwiftDataInBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataInBackgroundApp.swift
+//  SwiftDataInBackground
+//
+//  Created by sam hastings on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataInBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
